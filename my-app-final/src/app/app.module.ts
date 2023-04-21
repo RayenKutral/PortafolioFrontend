839d6c5151accc8c248dcfda06846830b6a2e973
Florencia Vega <florvega1598@gmail.com>
2023-04-21 09:50:13 -0300

    creacion carpeta frontend
    Se creo la carpeta frontend proyecto final

---
+++ my-app-final/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { ImagenesComponent } from './componentes/imagenes/imagenes.component';
+import { BannerComponent } from './componentes/banner/banner.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HysComponent } from './componentes/hys/hys.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    ImagenesComponent,
+    BannerComponent,
+    AcercaDeComponent,
+    ExperienciaComponent,
+    HysComponent,
+    ProyectosComponent,
+    FooterComponent
+  ],
+  
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
